eaec668aa5822d1b88d28c480c699c4bac7d511b
Andrei Burvel <burvelandrei@gmail.com>
2024-06-26 22:04:04 +0300

    V 1.3 немного обновил

--- downloader.py
+++ downloader.py
@@ -7,13 +7,18 @@ link = input("Введите ссылку на клип: ")
 
 main_path = "B:\Projests\Downloader_twitch_clips\loaded_clips"
 
-response = requests.get(link)
+def request_twitch():
+    response = requests.get(link)
 
-if response.status_code == 200:
-    page = BeautifulSoup(response.text, "html.parser")
-    # print(page)
-    # print(response.text)
-    link_clip = page.findAll("div", class_ = "Layout-sc-1xcs6mc-0 isUGlE")
-    print(link_clip)
-else:
-    print(f"Неверный статус - {response.status_code}")
+    if response.status_code == 200:
+        page = BeautifulSoup(response.text, "html.parser")
+        # print(page)
+        # print(response.text)
+        link_clip = page.findAll("div", class_ = "Layout-sc-1xcs6mc-0 isUGlE")
+        print(link_clip)
+    else:
+        print(f"Неверный статус - {response.status_code}")
+
+
+if __name__=="__main__":
+    request_twitch()
